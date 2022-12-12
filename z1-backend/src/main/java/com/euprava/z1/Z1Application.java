cadd00dd23d267e583cccd1fa55275b8ca1f89b5
Andrej <krtolinat@gmail.com>
2022-12-12 14:22:37 +0100

    Rename

--- z1-backend/src/main/java/com/euprava/z1/Z1Application.java
+++ z1-backend/src/main/java/com/euprava/z1/Z1Application.java
@@ -1,4 +1,4 @@
-package com.example.z1;
+package com.euprava.z1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- z1-backend/src/test/java/com/euprava/z1/Z1ApplicationTests.java
+++ z1-backend/src/test/java/com/euprava/z1/Z1ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.z1;
+package com.euprava.z1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
