0f67ad9a22e29b86c29f5de22bd1c2e1ec5b5d70
Andrej <krtolinat@gmail.com>
2022-12-12 14:18:21 +0100

    Added z1 backend

---
+++ z1-backend/src/main/java/com/example/z1/Z1Application.java
@@ -0,0 +1,13 @@
+package com.example.z1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Z1Application.class, args);
+    }
+
+}
