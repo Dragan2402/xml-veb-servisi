c7225a637ac2bf0a9b9b681b831465ce8062d413
Dragan2402 <draganmirkovic2402@gmail.com>
2023-01-11 14:43:43 +0100

    setting up fuseki

--- a1-backend/src/main/java/com/euprava/euprava/EupravaApplication.java
+++ a1-backend/src/main/java/com/euprava/euprava/EupravaApplication.java
@@ -1,5 +1,14 @@
 package com.euprava.euprava;
 
+
+import org.apache.jena.fuseki.build.FusekiConfig;
+import org.apache.jena.fuseki.main.FusekiServer;
+import org.apache.jena.fuseki.system.FusekiLogging;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.tdb.TDBFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +16,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EupravaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EupravaApplication.class, args);
+
+        Dataset ds = DatasetFactory.createTxnMem() ;
+        FusekiServer fusekiServer = FusekiServer.create()
+                .port(3031)
+                .add("/a1",ds)
+                .build();
+        FusekiLogging.setLogging();
+        fusekiServer.start();
     }
 
 }
